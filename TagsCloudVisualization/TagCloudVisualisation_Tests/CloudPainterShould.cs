b713b9c63b3d71d06063439f28bfc04a9fea5d43
KotlyarovV <vkotl@yandex.ru>
2017-12-10 18:40:11 +0500

    +verify tests

--- TagsCloudVisualization/TagCloudVisualisation_Tests/CloudPainterShould.cs
+++ TagsCloudVisualization/TagCloudVisualisation_Tests/CloudPainterShould.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Castle.Components.DictionaryAdapter.Xml;
 using Moq;
 using NUnit.Framework;
 using TagsCloudVisualization;
